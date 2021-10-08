48e52a92e41053523e1aab1e1a5efe92dc3c2aa5
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-10-08 17:29:34 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -148,7 +148,7 @@ async def cb_handler(client: Mai_bOTs , query: CallbackQuery):
             [
                 [
                     InlineKeyboardButton('Back', callback_data = "ghelp"),
-                    InlineKeyboardButton("Shut Down*, callback_data = "close")
+                    InlineKeyboardButton("Shut Down", callback_data = "close")
                 ]
             ]
         )
